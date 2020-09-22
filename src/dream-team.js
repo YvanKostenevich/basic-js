8e7c107f5d27eaf064851017c7291a69eebb12e9
Yvan <yvankostenevich@gmail.com>
2020-09-22 22:42:37 +0300

    feat: solved dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,22 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(array) {
+
+    if (Array.isArray(array) === false){
+        return false;
+    }
+
+    let stringArray = array.filter(value => typeof value === 'string');
+    let dreamTeamName = '';
+
+    for (let i = 0; i < stringArray.length; i++) {
+        stringArray[i] = stringArray[i].trim().toUpperCase();
+    }
+    stringArray.sort();
+    for (let i = 0; i < stringArray.length; i++) {
+
+        dreamTeamName = dreamTeamName + stringArray[i][0];
+
+    }
+
+    return dreamTeamName;
+
 };
